a62c345248a5d718def9272a7ad23bf05e6c5ec4
Dmitry Belichenko <mitterr@59.141.20.172.in-addr.arpa>
2020-06-13 16:48:11 +0300

    refactored

--- np_algo.cpp
+++ np_algo.cpp
@@ -1,5 +1,6 @@
 #pragma GCC optimize("Ofast" , "sse4" , "sse3" , "sse2" , "unroll-loops")
 #include <iostream>
+#include <Python.h>
 #include <ctime>
 #include <random>
 #include <map>
@@ -9,6 +10,7 @@
 #include <bitset>
 #include <cstring>
 #include <math.h>
+#include <Python.h>
 #include <string>
 #include <set>
 #include <iomanip>
@@ -22,17 +24,21 @@
 using namespace std;
 #define pb push_back
 typedef long long ll;
-#define SZ 20005
+const int SZ = 25005;
 const int GEN_N = 7;
 const int GEN_K = 7;
 vector<int> gr[25005];
-int a[25005];
-int f[25005][SZ]; // f[v][c] is a maximum number of happy vertices in subtree of v if color of v is c
-int g[25005][SZ]; // g[v][c] is a maximum number of happy vertices in subtree of v if color of v is c and is v is happy
-int mf[25005]; // max value of f over all colors
+int a[SZ];
+int f[SZ][SZ]; // f[v][c] is a maximum number of happy vertices in subtree of v if color of v is c
+int g[SZ][SZ]; // g[v][c] is a maximum number of happy vertices in subtree of v if color of v is c and is v is happy
+int Th[SZ][SZ]; // Th[v][c] is a maximum number of happy vertices in subtree of v if color of v is c and is v is happy
+int Tu[SZ][SZ]; // Tu[v][c] is a maximum number of happy vertices in subtree of v if color of v is c and is v is unhappy
+int Tall[SZ][SZ]; // Tall[v][c] is a maximum number of happy vertices in subtree of v if color of v is c and is v is unhappy
+
+int mf[SZ]; // max value of f over all colors
 // answer is maximum number of happy vertices = max(f[root][c] , g[root][c]) over all c
 int n , k;
-void dfs(int u , int p = -1)
+void solve_nk(int u , int p = -1)
 {
     // cout << u << "\n";
     if(p != -1)
@@ -41,7 +47,7 @@ void dfs(int u , int p = -1)
     }
     for(int v : gr[u])
     {
-        dfs(v , u);
+        solve_nk(v , u);
     }
     for(int i = 0; i < k; i++)
     {
@@ -64,12 +70,48 @@ void dfs(int u , int p = -1)
             g[u][i] += max(f[v][i] , g[v][i]);
         }
     }
-    //    sort(g[u], g[u] + k);
-    //    sort(f[u], f[u] + k);
     for(int i = 0; i < k; i++)
     {
         mf[u] = max(f[u][i] , mf[u]);
     }
+}
+
+
+void solve_nk_log_k(int u , int p = -1)
+{
+    if(p != -1)
+    {
+        gr[u].erase(find(gr[u].begin() , gr[u].end() , p));
+    }
+    for(int v : gr[u])
+    {
+        solve_nk_log_k(v , u);
+    }
+    for(int i = 0; i < k; i++)
+    {
+        if(a[u]>= 0 && a[u] != i)
+        {
+            continue;
+        }
+        Th[u][i] = 1;
+        Tu[u][i] = 0;
+    }
+    for(int v : gr[u])
+    {
+        for(int i = 0; i < k; i++)
+        {
+            if(a[u]>= 0 && a[u] != i)
+            {
+                continue;
+            }
+            Th[u][i] += max(Th[0][i], Tu[v][i]);
+            Tu[u][i] += max(Th[v][i], Tu[v][i]);
+            Tall[u][i] = max(Th[u][i], Tu[v][i]);
+            
+        }
+    }
+    sort(Th[u], Th[u] + k); // sort to get ordered values to get fast maximum
+    sort(Tu[u], Tu[u] + k);  // sort to get ordered values to get fast maximum
 }
 
 vector<int> get_mask(int j)
@@ -91,6 +133,7 @@ vector<int> get_mask(int j)
     }
     return mask;
 }
+
 bool try_dfs(int u , vector<int> &cl )
 {
     bool flag = 1;
@@ -127,6 +170,7 @@ void gen_tree(int n)
         gr[i - 1].pb(p);
     }
 }
+
 void relax(int n, int k)
 {
     for(int i = 0; i < n + 1; i++)
@@ -138,11 +182,10 @@ void relax(int n, int k)
         for(int j = 0; j < k + 1; j++)
             g[i][j] = f[i][j] = mf[i] = -1;
 }
+
 void gen(int n , int k)
 {
     relax(n, k);
-    //    n = rand() % GEN_N + 1;
-    //    k = rand() % GEN_K + 1;
     for(int i = 0; i < n; i++)
     {
         a[i] = rand() % k;
@@ -150,46 +193,55 @@ void gen(int n , int k)
     }
     gen_tree(n);
 }
+
 vector<long double> tm;
 vector<int> ks;
-void get_time()
+
+pair<vector<double>, vector<double>> get_time_for_fixed_N(int n, int freq)
 {
-    int i = 25000;
-    for(int j = 1; j <= SZ; j += 50)
+    int i = n;
+    vector<double> times_nk, times_nk_log_k;
+    for(int j = 1; j <= SZ; j += freq)
     {
         n = i;
         k = j;
         gen(i,j);
         double clo = (double)(clock());
         long double prev_time = (double) ((clock()) / (double)CLOCKS_PER_SEC) ;
-        dfs(0, -1);
+        solve_nk(0, -1);
         long double cur_time = (double) ((clock()) / (double)CLOCKS_PER_SEC) ;
-        if(prev_time > 0)
-        {
-            //cout << j << " " << cur_time - prev_time << "\n";
-            tm.pb(cur_time - prev_time);
-            ks.pb(j);
-        }
-        prev_time = cur_time ;
+        times_nk.pb(cur_time - prev_time);
+        prev_time = (double) ((clock()) / (double)CLOCKS_PER_SEC) ;
+        solve_nk_log_k(0, -1);
+        cur_time = (double) ((clock()) / (double)CLOCKS_PER_SEC) ;
+        times_nk_log_k.pb(cur_time - prev_time);
     }
-    cout << "[";
-    for(auto x : ks) cout << x << ",";
-    cout << "]";
-    cout << "\n";
-    cout << "[";
-    for(auto x : tm) cout << x << ",";
-    cout << "]";
-    cout << "\n";
+    return make_pair(times_nk, times_nk_log_k);
 }
-int main()
+
+void run_and_save_to_file(int n, int freq)
+{
+    freopen("input.txt","w", stdout);
+    auto timelines = get_time_for_fixed_N(n, freq);
+    for(auto ak: timelines.first) cout << vec << " ";
+    cout << "\n";
+    for(auto bk: timelines.first) cout << vec << " ";
+    cout << "\n" << freq << "\n";
+    return;
+}
+void run_py_stats_scripts()
+{
+    Py_SetProgramName(argv[0]);  /* optional but recommended */
+    Py_Initialize();
+    PyRun_SimpleString("'sys.path.append("r_square_mean.py")');
+    Py_Finalize();
+}
+int main(int argc,int argvx[])
 {
     srand(228);
-    get_time();
-    //cout << solve() << " ";
-    int ans = mf[0];
-    for(int i = 0; i < k; i++)
-    {
-        ans = max(ans , g[0][i]);
-    }
-    cout << ans << "\n";
+    n = atoi(argvx[1]);
+    int freq = atoi(argvx[2]);
+    run_and_save_to_file(n);
+    run_py_stats_scripts();
 }
+
